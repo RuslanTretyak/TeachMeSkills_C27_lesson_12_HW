4e4da7662044dbf37d153ead812c64853d0afdc7
Ruslan <ruslan.tretyak.94@gmail.com>
2024-02-15 14:46:51 +0300

    corrected task2 add README

---
+++ README.md
@@ -0,0 +1,8 @@
+## TeachMeSkills_C27_lesson_12_HW
+Homework for the twelfth lesson
+
+**task1** This program takes a string that the user enters from the keyboard and prints all the abbreviations from that 
+string to the console. An abbreviation is a word of 2 to 6 characters, consisting only of capital letters, without numbers.
+
+**task2** 
+
--- src/task2/Main.java
+++ src/task2/Main.java
@@ -1,14 +1,16 @@
 package task2;
 
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст");
-        Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
+        String str = "Программа на вход +(33)2879431 получает произвольный текст. В этом тексте может быть номер " +
+                "документа(один или несколько) 1423-1512-51, емейл teachmeskills@gmail.com и номер " +
+                "телефона +(29)2879431. Номер документа в формате: xxxx-xxxx-xx, где x - это любая цифра; номер " +
+                "телефона в формате: +(xx)xxxxxxx. Документ может содержать не 1423-1512-52 всю +(44)2879431 " +
+                "информацию teachme@gmail.com, т.е. например, может не содержать номер телефона, или другое. " +
+                "Необходимо найти эту meskills@gmail.com информацию и вывести в консоль в формате 1423-1512-53";
         findData(str);
     }
     private static void findData(String str) {
